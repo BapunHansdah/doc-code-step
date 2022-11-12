3879603d02a5373f9a9c958330cc2f029f62a950
Bapun <bapunhansdah777@gmail.com>
2022-11-12 17:49:56 +0530

    responsive

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -5,6 +5,9 @@ import {useSelector} from 'react-redux'
 import Folders from './files'
 import {Data} from './Data'
 import Modal from 'react-modal'
+import {GiHamburgerMenu} from 'react-icons/gi'
+// import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
+
 
 
 
@@ -34,6 +37,8 @@ function Main(){
     const [isSaved,setIsSaved] = useState(true)
     const [paths,setPaths] = useState("root/index.js")
     const [willImportfile,setWilImportFile] = useState(false)
+
+    const [showEditor , setShowEditor] = useState(false)
 
 
 
@@ -277,12 +282,22 @@ function Main(){
   }
 
 
+ function showEditorTab(){
+    setShowEditor(true)
+ }
+
+ function showFileTab(){
+    setShowEditor(false)
+ }
+
+ console.log(showEditor)
+
 	return(	
 		<>
            <div className="max-w-full h-screen bg-lime-200">
-              <div className="flex grid-cols-2 h-full shadow bg-black">
+              <div className="flex flex-col md:flex-row grid-cols-1 md:grid-cols-2 h-full shadow bg-black">
                 {/*leftbar*/}
-	           	   <div className="w-2/12 overflow-scroll scrollbar">
+	           	   <div className="md:w-2/12 overflow-scroll scrollbar flex flex-row md:flex-col">
 	           	     {
 	           	     	step.map((s,ind)=>{
 	           	     		return (
@@ -290,12 +305,12 @@ function Main(){
 	           	     			)
 	           	     	})
 	           	     }
-	           	      <div className="flex justify-center m-1 gap-1">
-	           	         <button onClick={openModal} className=" w-full text-white rounded bg-green-500 hover:bg-green-400">+</button>
-	           	         <button onClick={deleteStep} className={`w-full text-white rounded ${count ===1 ?"bg-red-200" : "bg-red-500"} hover:bg-red-400`} disabled={count ===1 ? true : false}>-</button>
+	           	      <div className="flex flex-row justify-center m-1 gap-1">
+	           	         <button onClick={openModal} className=" w-5 md:w-full text-white rounded bg-green-500 hover:bg-green-400">+</button>
+	           	         <button onClick={deleteStep} className={`w-5 md:w-full text-white rounded ${count ===1 ?"bg-red-200" : "bg-red-500"} hover:bg-red-400`} disabled={count ===1 ? true : false}>-</button>
 	           	      </div>    
 	           	   </div>
-	           	   <Modal className="max-w-[400px] w-[400px] mt-20 mx-auto bg-orange-500 rounded" isOpen={modal}>
+	           	   <Modal className="max-w-[400px] mt-20 mx-auto bg-orange-500 rounded" isOpen={modal}>
 	           	       <form className="flex flex-col p-5 gap-2" onSubmit={addStep}>
 	           	          <input onChange={addHandleChange} name='title' className="p-2" placeholder='title'/>
 	           	          <textarea onChange={addHandleChange} name='description' className="p-2" placeholder='description'/>
@@ -310,13 +325,19 @@ function Main(){
 	           	       </form>
 	           	   </Modal>
 	           	   {/*rightbar*/}
-	           	   <div className="w-10/12 pl-1">
+	           	   <div className="md:w-10/12 pl-1">
 	           	      <div className='bg-gray-900 p-3'>
 	           	         <div><h1 className="text-2xl font-bold text-white">{step[stepselect].title}</h1></div>
 	           	         <div><p className="text-sm text-gray-600">{step[stepselect].description}</p></div>
 	           	      </div>
+                      <div className="block md:hidden">
+                      <div className="text-white grid grid-cols-2 justify-center gap-2 ">
+                        <div className={`p-2 ${showEditor ? "":"bg-red-700"} `} onClick={showFileTab}>files</div>
+                        <div className={`p-2 ${showEditor ? "bg-red-700":""}`} onClick={showEditorTab}>editor</div>
+                      </div>
+                      </div>
 	           	      <div className="flex">
-	           	      	 <div className="bg-[#3d3d3d] w-full overflow-auto resize-x">
+	           	      	 <div className={`bg-[#3d3d3d] w-full overflow-auto resize-x ${showEditor?"hidden":""} md:block`}>
 	           	      	       <div className="font-bold text-white bg-black p-1 shadow border-r">Files</div>
 	           	      	       <div className="p-1">  	
 	           	      	       {          	      	       
@@ -324,26 +345,46 @@ function Main(){
 	           	      	       }                  	      	       
 	           	      	       </div>
 	           	      	 </div>
-	           	      	 <div className="bg-black w-full overflow-auto">
+	           	      	 <div className={`bg-black w-full overflow-auto ${!showEditor?"hidden":""} md:block`}>
 	           	      	      <div className="flex p-1 justify-between">
 	           	      	       <div className="text-white pl-5 flex items-center"><div className="flex gap-2 items-center text-xs">{'/'+paths}<div className={`w-2 h-2 rounded-full flex items-center ${isSaved? "":"bg-red-500"}`}></div></div></div>
-	           	      	       <div className="text-white pr-5 flex gap-2">
-	           	      	           {/*<button onClick={showFile} className="bg-gray-800 px-2 rounded">open</button>*/}
-	           	      	           <label htmlFor="file-upload" class="custom-file-upload bg-gray-800 px-2 rounded">
-                                       open
-                                   </label>
-	           	      	           <input className="text-sm" id="file-upload" type="file" name="file-upload" onChange={(e) => showFile(e)} />
-	           	      	           <button onClick={editCode} className="bg-gray-800 px-2 rounded">{!readonly?"Read" :"Edit"}</button>
-	           	      	           <button onClick={saveCode} className="bg-gray-800 px-2 rounded">Save</button> 
-	           	      	           <a download="filename.json"
-	           	      	                href={`data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(step))}`}
-	           	      	                className="bg-gray-800 px-2 rounded">Download
-	           	      	            </a> 
+	           	      	       <div className="hidden md:block">
+                                   <div className="text-white pr-5 flex gap-2">
+    	           	      	           {/*<button onClick={showFile} className="bg-gray-800 px-2 rounded">open</button>*/}
+    	           	      	           <label htmlFor="file-upload" class="custom-file-upload bg-gray-800 px-2 rounded">
+                                           open
+                                       </label>
+    	           	      	           <input className="text-sm" id="file-upload" type="file" name="file-upload" onChange={(e) => showFile(e)} />
+    	           	      	           <button onClick={editCode} className="bg-gray-800 px-2 rounded">{!readonly?"Read" :"Edit"}</button>
+    	           	      	           <button onClick={saveCode} className="bg-gray-800 px-2 rounded">Save</button> 
+    	           	      	           <a download="filename.json"
+    	           	      	                href={`data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(step))}`}
+    	           	      	                className="bg-gray-800 px-2 rounded">Download
+    	           	      	            </a> 
+
+    	           	      	       </div>
+                               </div>
+                               <div className="block md:hidden text-white pr-5 relative">
+                                  <div className="flex items-center"><GiHamburgerMenu size={20}/></div>
+                                  <div className="w-44 bg-black z-10 absolute flex justify-center">
+                                      <div className="text-white pr-5 flex-col flex gap-2 gap-2 w-full justify-center items-center">
+                                       <label htmlFor="file-upload" class="custom-file-upload w-full flex justify-center bg-gray-800 px-2 rounded">
+                                           open
+                                       </label>
+                                       <input className="text-sm flex justify-center w-full" id="file-upload" type="file" name="file-upload" onChange={(e) => showFile(e)} />
+                                       <button onClick={editCode} className="bg-gray-800 w-full px-2 rounded flex justify-center">{!readonly?"Read" :"Edit"}</button>
+                                       <button onClick={saveCode} className="bg-gray-800 w-full px-2 rounded flex justify-center">Save</button> 
+                                       <a download="filename.json"
+                                            href={`data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(step))}`}
+                                            className="bg-gray-800 px-2 rounded w-full flex justify-center">Download
+                                        </a> 
+                                   </div>
+                               
+                                  </div>
+                               </div>
 
 	           	      	       </div>
-
-	           	      	       </div>
-	           	      	       <div>
+	           	      	       <div className="">
 
 	           	      	          {/*<EditorContainer>*/}
 	           	      	                <Editor
